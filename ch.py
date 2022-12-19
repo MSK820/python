d0e26b6a385917f03a65e0b08824dd1d30406575
MSK820 <120168490+MSK820@users.noreply.github.com>
2022-12-19 13:06:51 +0530

    Delete ch.py

--- ch.py
+++
@@ -1,9 +0,0 @@
-ch=input("enter the char :")
-if (ch>="A" and ch<="Z"):
- print(" Given ch is a alpha")
-elif(ch>="a" and ch<="z"):
- print(" Given ch is a alpha")
-else:
- print(" Given ch is a alpha")
-
- 

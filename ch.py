e4b5d94a27b0f054b1ae066d14be44f81acf78c7
MSK820 <120168490+MSK820@users.noreply.github.com>
2022-12-19 13:07:29 +0530

    Add files via upload

---
+++ ch.py
@@ -0,0 +1,9 @@
+ch=input("enter the char :")
+if (ch>="A" and ch<="Z"):
+ print(" Given ch is a alpha")
+elif(ch>="a" and ch<="z"):
+ print(" Given ch is a alpha")
+else:
+ print(" Given ch is not a alpha")
+
+ 
